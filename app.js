93187dc03d2d7492b9b51ded8b07e50d042b825c
yasharjit <yasharjitm10@gmail.com>
2021-04-05 19:14:10 +0530

    remove unused code

--- app.js
+++ app.js
@@ -27,21 +27,6 @@ app.use("/api/v1/applications", applicationsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    res.setHeader(
-        "Access-Control-Allow-Origin",
-        "http://localhost:3000" || "https://mahajancyberworld.co.in"
-    );
-
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
-
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "X-Requested-With,content-type"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
     next(createError(404));
 });
 
