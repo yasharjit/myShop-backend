cf8fb8a37260c24a09fb29401e7fe11ef991a94f
yasharjit <yasharjitm10@gmail.com>
2021-04-07 23:11:27 +0530

    remove jade as a dep

--- app.js
+++ app.js
@@ -11,9 +11,6 @@ var applicationsRouter = require("./routes/applications");
 
 var app = express();
 
-// view engine setup
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "jade");
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
