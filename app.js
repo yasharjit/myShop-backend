60eea183c6c57f0127144da800d62498d1680292
yasharjit <yasharjitm10@gmail.com>
2021-04-20 22:21:01 +0530

    remove unused commented code

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var cors = require("cors");
-// var indexRouter = require("./routes/index");
-// var usersRouter = require("./routes/users");
 var applicationsRouter = require("./routes/applications");
 
 var app = express();
@@ -18,8 +16,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use("/", indexRouter);
-// app.use("/users", usersRouter);
 app.use("/api/v1/applications", applicationsRouter);
 
 // catch 404 and forward to error handler
